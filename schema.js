a3411f0f29552b3ddd219089c1825b99eff34b26
jerry <szy0syz@gmail.com>
2019-08-26 22:10:28 +0800

    init graphql to get ok

---
+++ schema.js
@@ -0,0 +1,48 @@
+// docs.spacexdata.com
+
+const {
+  GraphQLList,
+  GraphQLBoolean,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLObjectType,
+  GraphQLSchema,
+} = require('graphql');
+const axios = require('axios');
+
+const LaunchType = new GraphQLObjectType({
+  name: 'Launch',
+  fields: () => ({
+    flight_number: { type: GraphQLInt },
+    mission_name: { type: GraphQLString },
+    launch_year: { type: GraphQLString },
+    launch_date_local: { type: GraphQLString },
+    launch_success: { type: GraphQLBoolean },
+    rocket: { type: RocketType },
+  }),
+});
+
+const RocketType = new GraphQLObjectType({
+  name: 'Rocket',
+  fields: () => ({
+    rocket_id: { type: GraphQLString },
+    rocket_name: { type: GraphQLString },
+    rocket_type: { type: GraphQLString },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    launches: {
+      type: new GraphQLList(LaunchType),
+      resolve(parent, args) {
+        return axios.get('https://api.spacexdata.com/v3/launches').then(res => res.data)
+      }
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+})
