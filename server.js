bbc0a623379f4b208e59caa84b47be1c17457b04
jerry <szy0syz@gmail.com>
2019-08-26 22:30:15 +0800

    Update graphql = rocket

--- server.js
+++ server.js
@@ -8,4 +8,6 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true
 }));
 
-app.listen(4000);
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log(`Server started on http://127.0.0.1:${PORT}`));
